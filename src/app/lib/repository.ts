f03ffc2315e552215cd89ee66d171e58a9e39707
thepercival <coendunnink@gmail.com>
2022-07-19 20:11:51 +0200

    update repos

--- src/app/lib/repository.ts
+++ src/app/lib/repository.ts
@@ -1,13 +1,16 @@
 import { HttpParams } from '@angular/common/http';
 import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
-import { Observable, throwError as observableThrowError } from 'rxjs';
+import { Observable, throwError as observableThrowError, throwError } from 'rxjs';
 import { environment } from '../../environments/environment';
 
 export class APIRepository {
 
     protected apiurl: string = environment.apiurl;
+    private apiVersion: string = environment.apiVersion;
+    private appErrorHandler: AppErrorHandler;
 
     constructor() {
+        this.appErrorHandler = new AppErrorHandler();
     }
 
     getApiUrl(): string {
@@ -16,7 +19,7 @@ export class APIRepository {
 
     getHeaders(): HttpHeaders {
         let headers = new HttpHeaders({ 'Content-Type': 'application/json; charset=utf-8' });
-        headers = headers.append('X-Api-Version', '1');
+        headers = headers.append('X-Api-Version', this.apiVersion);
 
         const token = this.getToken();
         if (token !== undefined) {
@@ -38,24 +41,43 @@ export class APIRepository {
         };
     }
 
-    protected handleError(response: HttpErrorResponse): Observable<any> {
-        console.log(response);
-        let errortext;
+    protected handleError(error: HttpErrorResponse | Error): Observable<any> {
+        return this.appErrorHandler.handleError(error);
+    }
+}
+
+export class AppErrorHandler {
+    public handleError(error: HttpErrorResponse | Error): Observable<any> {
+        let errortext: string = 'onbekende fout(';
         if (!navigator.onLine) {
             errortext = 'er kan geen internet verbinding gemaakt worden';
-        } else if (typeof response === 'string') {
-            errortext = response;
-        } else if (response.status === 0) {
-            errortext = 'er kan geen verbinding met de data-service gemaakt worden, ververs de pagina';
-        } else if (response.status === 401) {
-            if (response.error && response.error.message === 'Expired token') {
-                errortext = 'de sessie is verlopen, log uit en log daarna opnieuw in';
-            } else {
-                errortext = 'je hebt geen rechten om de aanvraag te doen, waarschijnlijk is je sessie verlopen, log dan opnieuw in';
+        } else if (error instanceof HttpErrorResponse) {
+            console.log(error);
+            if (error.status === 0) {
+                errortext = 'er kan geen verbinding met de data-service gemaakt worden, ververs de pagina';
+            } else if (error.status === 401) {
+                if (error.error && error.error.message === 'Expired token') {
+                    errortext = 'de sessie is verlopen, log uit en log daarna opnieuw in';
+                } else {
+                    errortext = 'je hebt geen rechten om de aanvraag te doen, waarschijnlijk is je sessie verlopen, log dan opnieuw in';
+                }
+            } else if (error.error) {
+                if (typeof error.error === 'string') {
+                    errortext = error.error;
+                } else if (error.error.message) {
+                    errortext = error.error.message;
+                }
+            } else if (error && error.message) {
+                errortext = error.message;
             }
-        } else if (response.error && response.error.message) {
-            errortext = response.error.message;
         }
-        return observableThrowError(errortext);
+        else if (typeof error === 'string') {
+            errortext = error;
+        }
+        else {
+            errortext = error.message;
+        }
+        const err = new Error(errortext);
+        return throwError(() => err);
     }
 }
